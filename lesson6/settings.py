4a8d159e3498479872a4deca201268bdb550906e
viana <129645315+viana-alipanahifard@users.noreply.github.com>
2025-03-24 16:55:20 +0330

    Update settings.py

--- lesson6/settings.py
+++ lesson6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%=-)jki$$ck*m_-f1lk12c#xz^#)+843nlf0+5ug%@q0ltn)cq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
